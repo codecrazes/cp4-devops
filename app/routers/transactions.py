b33f11bbfc6d576a2e4d640c4bb4142488a4a6a1
AssisCarol <assiscaroline14@icloud.com>
2025-09-03 20:25:49 -0300

    feat: update db conection

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter, HTTPException
-from sqlalchemy.orm import Session
 
+from app.dependencies import DBSession
 from app.common.crud import create_transaction, delete_transaction, list_transactions, update_transaction
 from app.common.schemas import TransactionCreate, TransactionOut, TransactionUpdate
 
@@ -10,17 +10,17 @@ router = APIRouter(prefix="/transactions", tags=["Transactions"])
 
 
 @router.get("", response_model=List[TransactionOut])
-def get_transactions(db: Session):
+def get_transactions(db: DBSession):
     return list_transactions(db)
 
 
 @router.post("", response_model=TransactionOut, status_code=201)
-def post_transaction(payload: TransactionCreate, db: Session):
+def post_transaction(payload: TransactionCreate, db: DBSession):
     return create_transaction(db, payload)
 
 
 @router.put("/{tx_id}", response_model=TransactionOut)
-def put_transaction(tx_id: int, payload: TransactionUpdate, db: Session):
+def put_transaction(tx_id: int, payload: TransactionUpdate, db: DBSession):
     updated = update_transaction(db, tx_id, payload)
 
     if not updated:
@@ -30,7 +30,7 @@ def put_transaction(tx_id: int, payload: TransactionUpdate, db: Session):
 
 
 @router.delete("/{tx_id}", status_code=204)
-def del_transaction(tx_id: int, db: Session):
+def del_transaction(tx_id: int, db: DBSession):
     ok = delete_transaction(db, tx_id)
     
     if not ok:
